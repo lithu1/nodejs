2a67c1552ca820c0aae11acb087c4707b856d918
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-06-11 07:00:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hello from Node.js CI/CD Pipeline!');
 });
-
+  
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
     res.send('Hello from Node.js CI/CD Pipeline!');
